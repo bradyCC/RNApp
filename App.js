5da1f1cc9187d9564c87c3740700d9ea86c7cb3c
Brady <acheng_69@163.com>
2019-07-08 22:51:19 +0800

    react-navigation 兼容Android、IOS

--- App.js
+++ App.js
@@ -7,21 +7,25 @@
  */
 
 import React, { Fragment } from "react";
-import { SafeAreaView, StyleSheet, View, Text } from "react-native";
+import { SafeAreaView, Platform, StyleSheet, View, Text } from "react-native";
 import { Button } from "@ant-design/react-native"; // 集成ant-mobile-rn, 按需引入
 import AppNavigator from "./AppNavigator";
 
+const Navigator = Platform.select({
+  ios: () => (
+    <View style={{ flex: 1, backgroundColor: "#dbdbdb" }}>
+      <AppNavigator />
+    </View>
+  ),
+  android: () => (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#dbdbdb" }}>
+      <AppNavigator />
+    </SafeAreaView>
+  )
+})();
+
 const App = () => {
-  return (
-    <Fragment>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#dbdbdb" }}>
-        {/*<View style={styles.contaienr}>*/}
-        {/*  <Text>Welcome to React Native!</Text>*/}
-        {/*</View>*/}
-        <AppNavigator />
-      </SafeAreaView>
-    </Fragment>
-  );
+  return <Fragment>{Navigator}</Fragment>;
 };
 
 const styles = StyleSheet.create({
